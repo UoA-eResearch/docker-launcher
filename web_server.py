3c67951336f591288ff0f03649566f6250b8c05b
Nick Young <nick.young@auckland.ac.nz>
2017-07-12 18:07:02 +1200

    init

---
+++ web_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import docker
+from bottle import get, run, redirect, request
+
+image="rocker/tidyverse"
+internal_port="8787/tcp"
+
+client = docker.from_env()
+
+@get('/')
+def index():
+  # None = random host port
+  container = client.containers.run(image, ports={internal_port: None}, detach = True)
+  container.reload()
+  port = container.attrs['NetworkSettings']['Ports'][internal_port][0]['HostPort']
+  print("Spawned container on port {}. Container status: {}".format(port, container.status))
+  hostname = request.urlparts.netloc.replace(":" + str(request.urlparts.port), "")
+  redirect("http://{}:{}".format(hostname, port))
+
+run(host='0.0.0.0', port=3040)
