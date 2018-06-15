064ec7327307fe120052057c30cdee31cdd3025e
Nick Young <nick.young@auckland.ac.nz>
2018-06-15 12:08:39 +1200

    set timeout to 20m

--- web_server.py
+++ web_server.py
@@ -51,9 +51,9 @@ port = int(os.environ.get('PORT', 8080))
 if __name__ == "__main__":
   try:
     try:
-      run(host='0.0.0.0', port=port, debug=True, server='gunicorn', workers=8, reloader=True)
+      run(host='0.0.0.0', port=port, debug=True, server='gunicorn', workers=8, reloader=True, timeout=1200)
     except ImportError:
-      run(host='0.0.0.0', port=port, debug=True, reloader=True)
+      run(host='0.0.0.0', port=port, debug=True, reloader=True, timeout=1200)
   except Exception as e:
     logger.error(e)
     sys.stdin.readline()
